eb7e391df8c9f0dc2e0fb1dc9b9cb2c883628c76
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-08-22 05:48:39 +0900

    Layout と Header の作成

---
+++ src/pages/Top.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+function Top() {
+  return (
+    <Layout>
+      top page
+    </Layout>
+  )
+}
+
+export default Top
---
+++ src/pages/Watch.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+function 
+Watch() {
+  return (
+    <Layout>
+      Watch
+    </Layout>
+  )
+}
+
+export default Watch
